266e8ee7bbe5525214eb03ea7dfa09220375ee94
Danilo Rosado <66913651+Danilo-mr@users.noreply.github.com>
2021-03-31 07:48:47 -0300

    modi

--- SelectSort/SelectSort.cpp
+++ SelectSort/SelectSort.cpp
@@ -4,33 +4,15 @@
 
 using namespace std;
 
-int qtd_comp, qtd_trocas;
+#define n 20
 
-#define n 30
+long long int qtd_comp=0, qtd_trocas=0;
+double duracao;
 
-double selection(int v[]){
-    clock_t start, finish;
-    int i, j, aux, menor, escolhido, flag; 
-    menor=v[0], qtd_comp=0, qtd_trocas=0;
-    start=clock();
-    for(j=0; j<n-1; j++){
-        for(i=n-1; i>j; i--){
-            qtd_comp++;
-            if(v[i]<menor)
-            {
-                escolhido=i;
-                flag=1;
-            }
-        }
-        if(flag==1){
-            qtd_trocas++;
-            aux=menor;
-            menor=v[escolhido];
-            v[escolhido]=menor;
-        }
-    }
-    finish=clock();
-    return (double)(finish-start)/CLOCKS_PER_SEC; 
+void imprimir (int v[]){
+    int i;
+    for(i=0; i<n; i++)
+        cout << v[i] << " ";
 }
 
 void vetorAleatorio(int v[]){
@@ -51,37 +33,60 @@ void vetorInverso(int v[]){
         v[i] = n-i;
 }
 
-void resultado(double d){
-    cout << "\n\nComparacoes: " << qtd_comp;
-    cout << "\nTrocas: " << qtd_trocas;
-    cout << "\nTempo de execucao: "  << d << "s"; 
+void selection(int v[]){
+    qtd_comp=0, qtd_trocas=0;
+    clock_t start, finish;
+    int i, j, pos, aux;
+
+
+    start=clock();
+    for(i=0; i<n-1; i++)
+    {
+        pos=i;
+        for(j=i+1; j<n; j++)
+        {
+            qtd_comp++;
+            if(v[j]<v[pos])
+                pos=j;
+        }
+        if(pos!=i){
+            qtd_trocas++;
+            aux=v[pos];
+            v[pos]=v[i];
+            v[i]=aux;
+        }
+    }
+    finish=clock();
+    duracao = (double)(finish-start)/CLOCKS_PER_SEC; 
 }
 
-void imprimir(int v[]) {
-    int i;
-    for (i=0; i<n; i++){
-        cout << v[i] << endl;
-    }
+void resultado(){
+    cout << "\n\n\nComparacoes: " << qtd_comp;
+    cout << "\nTrocas: " << qtd_trocas;
+    cout << "\nTempo de execucao: "  << duracao << "s"; 
+    cout << "\n\n\n";
 }
+
+
 
 int main() {
+
+
     int v[n];
-    double duracao;
 
-    cout << "\n\nVetor preenchido aleatoriamente";
-    vetorAleatorio(v);
-    duracao=selection(v);
-    resultado(duracao);
-    imprimir(v);
-
-    cout << "\n\nVetor Inversamente preenchido";
+    cout << "\n\n\n\t\t\tVETOR INVERSO...";
     vetorInverso(v);
-    duracao=selection(v);
-    resultado(duracao);
+    selection(v);
+    resultado();
 
-    cout << "\n\nVetor previamente ordenado";
+    cout << "\n\n\n\t\t\tVETOR ALEATORIO...";
+    vetorAleatorio(v);
+    selection(v);
+    resultado();
+
+    cout << "\n\n\n\t\t\tVETOR ORDENADO...";
     vetorOrdenado(v);
-    duracao=selection(v);
-    resultado(duracao);
-   
+    selection(v);
+    resultado();
+
 }
